3bc9591e3a3a578e2d92fa40187f66d40d65e98c
1491807832@qq.com <kouliubo0406>
2019-07-08 18:38:54 +0800

    最短路程

--- app/src/test/java/net/lzzy/algorithm/algorlib/BusMapTestTest.java
+++ app/src/test/java/net/lzzy/algorithm/algorlib/BusMapTestTest.java
@@ -1,5 +1,7 @@
 package net.lzzy.algorithm.algorlib;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
@@ -7,5 +9,24 @@ import static org.junit.Assert.*;
  * Description:
  */
 public class BusMapTestTest {
+    @Test
+    public void iterateDEpthFirst() {
+        BusMap map = new BusMap(5);
+        map.setStart(0);
+        map.setTarget(4);
+        map.addTwoWayEdge(0, 4, 10);
+        map.addTwoWayEdge(0, 1, 2);
+        map.addTwoWayEdge(0, 2, 4);
+        map.addTwoWayEdge(1, 4, 7);
+        map.addTwoWayEdge(1, 2, 3);
+        map.addTwoWayEdge(2, 3, 4);
+        map.addTwoWayEdge(2, 4, 3);
+        map.addTwoWayEdge(3, 4, 5);
+        int expected = 7;
+        assertEquals(expected, (int) map.getMinDistance(BusMap.KEY_FLOYO));
+
+
+    }
+
 
 }
